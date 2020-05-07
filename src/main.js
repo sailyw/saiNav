791907cb8db2a45eae18533185ff94630fe3b967
sailyw <1152827086@qq.com>
2020-05-07 10:43:49 +0800

    add key

--- src/main.js
+++ src/main.js
@@ -3,24 +3,40 @@ const $lastLi = $siteList.find("li.last");
 const x = localStorage.getItem("x");
 const xObject = JSON.parse(x);
 const hasMap = xObject || [
-  { logo: "A", logoType: "text", url: "https://www.acfun.cn" },
-  {
-    logo: "./images/bilibili.png",
-    logoType: "image",
-    url: "https://www.bilibili.com/",
-  },
+  { logo: "A", url: "https://www.acfun.cn" },
+  { logo: "B", url: "https://www.bilibili.com/" },
 ];
+//去除链接前面的https/http/www.
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(/\/.*/, ""); //删除/开头的内容
+};
+
 const render = () => {
   $siteList.find("li:not(.last)").remove();
-  hasMap.forEach((node) => {
+  hasMap.forEach((node, index) => {
     const $li = $(`<li>
-					<a href="${node.url}">
-						<div class="site">
-							<div class="logo">${node.logo[0]}</div>
-							<div class="link">${node.url}</div>
-						</div>
-					</a>
-				</li>`).insertBefore($lastLi);
+			<div class="site">
+				<div class="logo">${node.logo}</div>
+				<div class="link">${simplifyUrl(node.url)}</div>
+				<div class="close">
+					<svg class="icon">
+						<use xlink:href="#icon-close"></use>
+					</svg>
+				</div>
+			</div>
+    </li>`).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); //阻止冒泡
+      hasMap.splice(index, 1);
+      render();
+    });
   });
 };
 render();
@@ -33,8 +49,7 @@ $(".addButton").on("click", () => {
   }
   console.log(url);
   hasMap.push({
-    logo: url[0],
-    logoType: "text",
+    logo: simplifyUrl(url)[0].toUpperCase(),
     url: url,
   });
   render();
@@ -43,3 +58,11 @@ window.onbeforeunload = () => {
   const string = JSON.stringify(hasMap);
   localStorage.setItem("x", string);
 };
+$(document).on("keypress", (e) => {
+  const { key } = e;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url);
+    }
+  }
+});
